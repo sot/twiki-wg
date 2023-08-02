6135f0cc6a019ad44208399e1ccfc0986cbfd326
Jean Connelly <jconnelly@cfa.harvard.edu>
2023-08-02 11:24:08 -0400

    Python package this

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+entry_points = {
+    "console_scripts": [
+        "twiki_wg_make_wg_index=twiki_wg.make_wg_index:main",
+        "twiki_wg_ssawg_trending_scraper=twiki_wg.ssawg_trending_scraper:main"
+    ]
+}
+
+setup(
+    name="twiki_wg",
+    author="John Scott III, Tom Aldcroft",
+    description="Twiki trending and scraping",
+    author_email="taldcroft@cfa.harvard.edu",
+    use_scm_version=True,
+    setup_requires=["setuptools_scm", "setuptools_scm_git_archive"],
+    zip_safe=False,
+    entry_points=entry_points,
+    packages=["twiki_wg"],
+    package_data={
+        "twiki_wg": ["data/*.html", "task_schedule.cfg"]
+    },
+)
---
+++ twiki_wg/__init__.py
@@ -0,0 +1,8 @@
+# Licensed under a 3-clause BSD style license - see LICENSE.rst
+"""
+twiki_wg is a package that includes helper scripts for reviewing
+trending plots and finding twiki pages.
+"""
+from ska_helpers.version import get_version
+
+__version__ = get_version(__package__)
