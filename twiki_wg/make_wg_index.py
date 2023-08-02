0db1dd39d2f28abbe1053886ae1e8be6ee629170
Jean Connelly <jconnelly@cfa.harvard.edu>
2023-08-02 11:32:29 -0400

    Modernize a bit and get rid of globals

--- twiki_wg/make_wg_index.py
+++ twiki_wg/make_wg_index.py
@@ -5,29 +5,30 @@ import os
 import argparse
 import requests
 import bs4
-import Ska.ftp
+import ska_ftp
 import re
 
 
 TWIKI_URL = 'https://occweb.cfa.harvard.edu/twiki/bin/view/'
 
-netrc = Ska.ftp.parse_netrc()
-if 'occweb' not in netrc:
-    raise RuntimeError('must have occweb auth in ~/.netrc')
 
-user = netrc['occweb']['login']
-password = netrc['occweb']['password']
+def get_user_password():
+    netrc = ska_ftp.parse_netrc()
+    if 'occweb' not in netrc:
+        raise RuntimeError('must have occweb auth in ~/.netrc')
 
-# Dummy soup for making new tags
-soup = bs4.BeautifulSoup('', 'lxml')
+    user = netrc['occweb']['login']
+    password = netrc['occweb']['password']
+    return user, password
 
 
-def get_twiki_page(page):
+def get_twiki_page(page, working_group_web):
     """
     Get and parse a TWiki page.
     """
-    url = TWIKI_URL + opt.working_group_web + '/' + page
+    url = TWIKI_URL + working_group_web + '/' + page
     print('Reading {} twiki page'.format(url))
+    user, password = get_user_password()
     r = requests.get(url, auth=(user, password))
     out = bs4.BeautifulSoup(r.text, 'lxml')
 
@@ -83,6 +84,9 @@ def get_other_notebooks(agenda_div, meeting_page):
             other_nbs.append((href, tag))
 
     if other_nbs:
+        # Dummy soup for making new tags
+        soup = bs4.BeautifulSoup('', 'lxml')
+
         out = soup.new_tag('div')
 
         h3 = soup.new_tag('h3')
@@ -131,18 +135,14 @@ def get_opt():
                         default='StarWorkingGroup',
                         type=str,
                         help='Meeting archive index page')
-
-    args = parser.parse_args()
-
-    return args
+    return parser
 
 
-def main():
+def main(args=None):
     # The output of this process is a summary page named ``agendas_filename`` with
     # all the agenda sections glopped together.  First read and parse the existing
     # page.
-    global opt
-    opt = get_opt()
+    opt = get_opt().parse_args(args=args)
 
     agendas_filename = os.path.join(opt.data_dir, opt.index_file)
     agendas_page = bs4.BeautifulSoup(open(agendas_filename).read(), 'lxml')
@@ -161,7 +161,7 @@ def main():
     # * StarWorkingGroupMeeting2018x05x02
     # * StarWorkingGroupMeeting2018x04x18
 
-    meeting_index = get_twiki_page(opt.meeting_index_page)
+    meeting_index = get_twiki_page(opt.meeting_index_page, opt.working_group_web)
     meetings = find_tag(meeting_index, 'h2', 'Meeting')
 
     # Narrow down to the list (UL) of links to meeting notes
@@ -180,11 +180,14 @@ def main():
     # reference along with an <h2> title.
     for ii, new_link in enumerate(reversed(new_links)):
         meeting = new_link.text  # e.g. StarWorkingGroupMeeting2017x07x12
-        meeting_page = get_twiki_page(meeting)
+        meeting_page = get_twiki_page(meeting, opt.working_group_web)
         try:
             meeting_agenda_ul = get_list_after(meeting_page, 'h2', r'Agenda')
         except:
             meeting_agenda_ul = 'No agenda'
+
+        # Dummy soup for making new tags
+        soup = bs4.BeautifulSoup('', 'lxml')
 
         # Make the new <div> with an enclosed <h2> plus agenda items
         agenda_div = soup.new_tag('div', id=meeting)
