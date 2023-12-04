544cef81190efa371c1bb54748fb02a46ad92a2b
Tom Aldcroft <taldcroft@gmail.com>
2023-12-04 04:34:22 -0500

    Add script to compare two TWiki pages

--- setup.py
+++ setup.py
@@ -4,13 +4,14 @@ entry_points = {
     "console_scripts": [
         "twiki_wg_make_wg_index=twiki_wg.make_wg_index:main",
         "twiki_wg_ssawg_trending_scraper=twiki_wg.ssawg_trending_scraper:main",
+        "twiki_make_diff = twiki_wg.make_diff:main",
     ]
 }
 
 setup(
     name="twiki_wg",
     author="John Scott III, Tom Aldcroft",
-    description="Twiki trending and scraping",
+    description="Twiki trending, scraping, and diffing",
     author_email="taldcroft@cfa.harvard.edu",
     use_scm_version=True,
     setup_requires=["setuptools_scm", "setuptools_scm_git_archive"],
